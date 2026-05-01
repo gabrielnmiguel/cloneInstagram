a5188ac3c45038bb8af3e5e82af28fe9ae5a98bd
Grazielly Lacerda <grazielly.glacerda@senacsp.edu.br>
2026-04-30 22:05:44 -0300

    Refactor follow button functionality and layout

--- src/components/sugestão/index.jsx
+++ src/components/sugestão/index.jsx
@@ -1,24 +1,24 @@
 import { useState } from "react"
- 
+
 export default function Sugest(props){
     const [seguirText, setSeguirText] = useState('Seguir');
- 
+
     function Follow(){
         if (seguirText === 'Seguir') {
- 
+
             setSeguirText('Seguindo');
         } else {
             setSeguirText("Seguir");
         }
     }
- 
+
     return (
         <div className="sugest">
-            <div>
+            <div className="perfil">
                 <img className="photo" src={props.foto} alt={props.nome}></img>
                 <p>{props.nome}</p>
+                </div>
                 <button onClick={Follow}>{seguirText}</button>
-            </div>
         </div>
     )
-}
+} 

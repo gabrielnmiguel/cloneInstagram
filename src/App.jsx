939d3a59a08e9679957be7323182c55e3da66591
Will Will <seuemail@email.com>
2026-04-30 22:02:58 -0300

    atualizando componentes e adicionando imagens

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
-
 import './App.css'
-// import Sidebar from './components/navigate/index.jsx'
-import Story from './components/story/Story.jsx'
+import  Story from './components/story/Story'
+import Will from './assets/eu carton.png'
+import Gabi from './assets/salsicha.jpg'
+import Grazim from './assets/valente.jpg'
+import Meu from './assets/meu.jpg'
+import Jeto from './assets/JETP.jpg'
+
 
 function App() {
 
 
   return (
     <>
-     <Story> 
-
-    </Story>
+    <Story
+      fotos={Gabi}
+      fotos1={Grazim}
+      fotos2={Meu}
+      fotos3={Jeto}
+      fotos4={Will}
+      title="Usuarios"
+      perfil1="Gabriel"
+      perfil2="Grazi"
+      perfil3="iago"
+      perfil4="jetomi"
+      perfil5="William"
+    />
     </>
   )
 }
--- src/components/story/Story.jsx
+++ src/components/story/Story.jsx
@@ -1,19 +1,37 @@
+import './style.css'
 
-export default function story(props){
+export default function Story(props){
     return (
         <div className="container">
 
             <div className="imagens">
-            <img src="{props.fotos}" alt="{props.title}" className="fotos" />
+
+            <div className="item">
+            <img src={props.fotos} alt={props.title} className="fotos" />
             <p className="perfil">{props.perfil1}</p>
-            <img src="{props.fotos}" alt="{props.title}" className="fotos" />
-             <p className="perfil2">{props.perfil2}</p>
-            <img src="{props.fotos}" alt="{props.title}" className="fotos" />
-             <p className="perfil3">{props.perfil3}</p>
-            <img src="{props.fotos}" alt="{props.title}" className="fotos" />
-             <p className="perfil4">{props.perfil4}</p>
             </div>
 
+            <div className="item">
+            <img src={props.fotos1} alt={props.title} className="fotos" />
+            <p className="perfil2">{props.perfil2}</p>
+            </div>
+
+             <div className="item">
+            <img src={props.fotos2} alt={props.title} className="fotos" />
+             <p className="perfil3">{props.perfil3}</p>
+             </div>
+
+            <div className="item">
+            <img src={props.fotos3} alt={props.title} className="fotos" />
+             <p className="perfil4">{props.perfil4}</p>
+             </div>
+
+             <div className="item">
+            <img src={props.fotos4} alt={props.title} className="fotos" />
+            <p className="perfil5">{props.perfil5}</p>
+            </div>
+
+            </div>
 
         </div>
 

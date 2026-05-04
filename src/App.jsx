0e0aefdea9babfd459a2e80aee492c75f6fc26cd
GABRIEL DO NASCIMENTO MIGUEL <gabriel.nmiguel@senacsp.edu.br>
2026-05-04 20:16:06 -0300

    Teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css'
 import  Story from './components/story/Story'
 import Sugest from  './components/sugestão/index.jsx'
@@ -14,7 +13,6 @@ import Carinha2 from './assets/sugestão/carinha 2.jpg'
 import Carinha3 from './assets/sugestão/carinha 3.jpg'
 
 
-=======
 import { useState } from 'react'
 import './reset.css'
 import './App.css'
@@ -27,42 +25,23 @@ import { Sidebar as Side } from './components/navigate/index'
 import {Post} from './components/post/index'
 import { FloatMessagens } from './components/floatMessages/index'
 import { Footer } from './components/footer/index'
->>>>>>> sidebar
 
 function App() {
 
 
   return (
     <>
-<<<<<<< HEAD
-     <div className="layout">
+     {/* <div className="layout">
     <section className="inicio">
-    <Story
-      fotos={Gabi}
-      fotos1={Grazim}
-      fotos2={Meu}
-      fotos3={Jeto}
-      fotos4={Will}
-      title="Usuarios"
-      perfil1="Gabriel"
-      perfil2="Grazi"
-      perfil3="iago"
-      perfil4="jetomi"
-      perfil5="William"
-    />
+
     </section>
   <section className="sugestao">
-    <Sugest foto= {Garota1} nome='Gabrielle Rodrigues'></Sugest>
-    <Sugest foto= {Carinha1} nome='Davi Brito'></Sugest>
-    <Sugest foto= {Garota2} nome='Ana T.'></Sugest>
-    <Sugest foto= {Carinha2} nome='Marcos Henrique'></Sugest>
-    <Sugest foto= {Carinha3} nome='Thiago Mariano'></Sugest>
+
   </section>
   </div>
-    </>
-    
-  )
-=======
+
+     */}
+  
       <header>
         <Side />
 
@@ -74,7 +53,19 @@ function App() {
 
           </section>
           <section className='pageContent'>
-
+            <Story
+              fotos={Gabi}
+              fotos1={Grazim}
+              fotos2={Meu}
+              fotos3={Jeto}
+              fotos4={Will}
+              title="Usuarios"
+              perfil1="Gabriel"
+              perfil2="Grazi"
+              perfil3="iago"
+              perfil4="jetomi"
+              perfil5="William"
+            />
             <Post></Post>
           </section>
 
@@ -89,7 +80,11 @@ function App() {
           </section>
 
           <section className='suggestionSection'>
-
+            <Sugest foto= {Garota1} nome='Gabrielle Rodrigues'></Sugest>
+            <Sugest foto= {Carinha1} nome='Davi Brito'></Sugest>
+            <Sugest foto= {Garota2} nome='Ana T.'></Sugest>
+            <Sugest foto= {Carinha2} nome='Marcos Henrique'></Sugest>
+            <Sugest foto= {Carinha3} nome='Thiago Mariano'></Sugest>
 
           </section>
 
@@ -104,7 +99,6 @@ function App() {
       </div>
     </>
   );
->>>>>>> sidebar
 }
 
 export default App

ae0f2403ae103119bdb02ae29caa66b421b8cd99
will <william.sprjesus@senacsp.edu.br>
2026-05-08 10:24:09 -0300

    minha alteração

--- src/components/post/index.jsx
+++ src/components/post/index.jsx
@@ -1,24 +1,38 @@
 import './style.css'
-
-
+import { useState } from 'react';
 
 import { IonIcon } from '@ionic/react';
-import { ellipsisHorizontal, ellipsisHorizontalOutline, ellipsisHorizontalSharp } from 'ionicons/icons';
-import { person, personOutline, personSharp } from 'ionicons/icons';
-import { volumeHigh, volumeHighOutline, volumeHighSharp } from 'ionicons/icons';
-import { volumeMute, volumeMuteOutline, volumeMuteSharp } from 'ionicons/icons';
-import { heart, heartOutline, heartSharp } from 'ionicons/icons';
-import { chatbubble, chatbubbleSharp, chatbubbleOutline } from 'ionicons/icons';
-import { sync, syncOutline, syncSharp } from 'ionicons/icons';
-import { paperPlane, paperPlaneOutline, paperPlaneSharp } from 'ionicons/icons';
-import { bookmark, bookmarkOutline, bookmarkSharp } from 'ionicons/icons';
 
-import imagemPost  from '../../imgs/imagemPost.jpeg';
+import { ellipsisHorizontalOutline } from 'ionicons/icons';
+import { personOutline } from 'ionicons/icons';
+import { volumeHighOutline } from 'ionicons/icons';
+import { heartOutline } from 'ionicons/icons';
+import { chatbubbleOutline } from 'ionicons/icons';
+import { syncOutline } from 'ionicons/icons';
+import { paperPlaneOutline } from 'ionicons/icons';
+import { bookmarkOutline } from 'ionicons/icons';
+
+import imagemPost from '../../imgs/imagemPost.jpeg';
 import imagePerfil from '../../imgs/imagePerfil.png';
 
+export function Post(props) {
 
+    const [comentario, setComentario] = useState("");
+    const [aparecerComentario, setAparecerComentario] = useState(false);
 
-export function Post(props){
+    const [listaComentarios, setListaComentarios] = useState([
+        "Muito top 🔥",
+        "Post incrível 👏"
+    ]);
+
+    function adicionarComentario() {
+
+        if (comentario.trim() === "") return;
+
+        setListaComentarios([...listaComentarios, comentario]);
+
+        setComentario("");
+    }
 
     return (
         <section className='allPost'>
@@ -34,37 +48,49 @@ export function Post(props){
                         <section className='topItems'>
                             <section className='perfilImage'>
                                 <figure>
-                                    <img className='perfilImageItem' src={imagePerfil} alt="" srcset="" />
+                                    <img className='perfilImageItem' src={imagePerfil} alt="" />
                                     <figcaption></figcaption>
                                 </figure>
 
                             </section>
+
                             <section className='postPublisherInformation'>
                                 <p>
                                     <a className='publisherUsername' href="#">gabrielmiguel__</a>
                                     <a className='publicationTime' href="#">7h</a>
                                 </p>
+
                                 <p className='postAssets'>
-                                    <a href="https://www.youtube.com/watch?v=8qPTWzyVjKg">Original audio</a>
-                                s</p>
+                                    <a href="https://www.youtube.com/watch?v=8qPTWzyVjKg">
+                                        Original audio
+                                    </a>
+                                </p>
 
                             </section>
 
                             <section className='moreOptionsSection'>
-                                <IonIcon className='moreOptions' icon={ellipsisHorizontalOutline}></IonIcon>
-
+                                <IonIcon
+                                    className='moreOptions'
+                                    icon={ellipsisHorizontalOutline}
+                                />
                             </section>
 
                         </section>
 
                         <section className='bottomItems'>
+
                             <section className='markersProfilesSection'>
-                                <IonIcon className='markersProfiles' icon={personOutline}></IonIcon>
-
+                                <IonIcon
+                                    className='markersProfiles'
+                                    icon={personOutline}
+                                />
                             </section>
-                            <section className='audioStatusSection'>
-                                <IonIcon className='audioStatus' icon={volumeHighOutline}></IonIcon>
 
+                            <section className='audioStatusSection'>
+                                <IonIcon
+                                    className='audioStatus'
+                                    icon={volumeHighOutline}
+                                />
                             </section>
 
                         </section>
@@ -73,64 +99,143 @@ export function Post(props){
 
                 </section>
 
-
             </section>
 
-            <section className='postSecondPiece'>   
+            <section className='postSecondPiece'>
+
                 <section className='interactionIcons'>
+
                     <section className='interactionIconsFirstPart'>
+
                         <section className='heartIcon'>
-                            <IonIcon className='heart' icon={heartOutline} />
+                            <IonIcon
+                                className='heart'
+                                icon={heartOutline}
+                            />
+
                             <p className='value'>{100}</p>
-
                         </section>
-                        <section className='chatbubbleIcon'>
-                            <IonIcon className='chatbubble' icon={chatbubbleOutline} />
-                            <p className='value'>{200}</p>
 
+                        <section
+                            className='chatbubbleIcon'
+                            onClick={() => setAparecerComentario(true)}
+                        >
+                            <IonIcon
+                                className='chatbubble'
+                                icon={chatbubbleOutline}
+                            />
+
+                            <p className='value'>
+                                {listaComentarios.length}
+                            </p>
                         </section>
+
                         <section className='syncIcon'>
-                            <IonIcon className='sync' icon={syncOutline} />
+                            <IonIcon
+                                className='sync'
+                                icon={syncOutline}
+                            />
+
                             <p className='value'>{50}</p>
-
                         </section>
+
                         <section className='paperPlaneIcon'>
-                            <IonIcon className='paperPlane' icon={paperPlaneOutline} />
-
+                            <IonIcon
+                                className='paperPlane'
+                                icon={paperPlaneOutline}
+                            />
                         </section>
-                        
 
                     </section>
 
                     <section className='interactionIconsSecondPart'>
                         <section className='bookmarkIcon'>
-                            <IonIcon className='bookmark' icon={bookmarkOutline}></IonIcon>
-
+                            <IonIcon
+                                className='bookmark'
+                                icon={bookmarkOutline}
+                            />
                         </section>
-
-
                     </section>
 
                 </section>
 
                 <section className='postInfo'>
+
                     <section className='likes'>
-                        <p className='likesP'>like by <span>{'williamG'}</span> and <span>{100}</span> <span>others</span></p>
-
+                        <p className='likesP'>
+                            like by <span>williamG</span> and <span>100</span>
+                            <span> others</span>
+                        </p>
                     </section>
+
                     <section className='description'>
-                        <p>{'username'} {'verifictionIcon'} Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem repellat dolore molestiae error quam minus natus magnam quibusdam sunt id! Nam repudiandae iure eveniet impedit excepturi! Nihil neque praesentium doloremque. #teste #teste #teste #teste</p>
-
+                        <p>
+                            username verifictionIcon Lorem ipsum dolor sit,
+                            amet consectetur adipisicing elit.
+                        </p>
                     </section>
-
 
                 </section>
 
             </section>
 
+            {/* CAIXA DE COMENTÁRIOS */}
+
+            {
+                aparecerComentario && (
+
+                    <section className='comentariosModal'>
+
+                        <section className='comentariosBox'>
+
+                            <div className='comentariosTop'>
+
+                                <h2>Comentários</h2>
+
+                                <button
+                                    onClick={() => setAparecerComentario(false)}
+                                >
+                                    X
+                                </button>
+
+                            </div>
+
+                            <section className='listaComentarios'>
+
+                                {
+                                    listaComentarios.map((item, index) => (
+                                        <p key={index}>
+                                            <strong>usuario:</strong> {item}
+                                        </p>
+                                    ))
+                                }
+
+                            </section>
+
+                            <section className='adicionarComentario'>
+
+                                <input
+                                    type="text"
+                                    placeholder='Digite um comentário'
+                                    value={comentario}
+                                    onChange={(e) =>
+                                        setComentario(e.target.value)
+                                    }
+                                />
+
+                                <button onClick={adicionarComentario}>
+                                    Enviar
+                                </button>
+
+                            </section>
+
+                        </section>
+
+                    </section>
+
+                )
+            }
+
         </section>
-
-    )
-
-
+    );
 }
